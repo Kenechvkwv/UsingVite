7483f9316508af2f52a2706ec6c6ef6b627a1047
kenechvkwv <ubesiekene@gmail.com>
2022-11-02 10:34:30 +0100

    rolled back gh pages, scrapped

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://github-kenechvkwv.github.io/UsingVite/",
   plugins: [react()],
 });
